e98864ab30804d01d9a63d2b31668b8406fac091
kat tsukuda <BearHeartsBat@Kats-MBP.mhs>
2017-06-13 21:54:49 -0700

    remove reddit proxy from package.json and render with ajax

--- src/containers/Forum.js
+++ src/containers/Forum.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import Thread from '../components/Thread';
+import $ from 'jquery-ajax';
 
 class Forum extends Component {
   constructor(props) {
     super(props)
     this.state = {
-    thread_titles: ["Proper way to eat a pie?", "Cheese & Potato Bear Pie"]
+    threads: []
     }
   }
-  render(){
-    return(
+  componentDidMount() {
+    $.get("https://www.reddit.com/r/pancakes.json")       // request json from reddit
+    .then((res) => {                // wait for response...
+      console.log('bublaban', res)
+      this.setState({               // update internal state
+        threads: res.data.children
+      })
+    }
+    );
+  }
+  render() {
+    return (
       <div>
         <h1>The { this.props.params.forum_name } Subreddit</h1>
-        { this.state.thread_titles.map((title) => <Thread title={title} />) }
+        { this.state.threads.map((thread) => <Thread title={thread.data.title} />) }
       </div>
     );
   }
